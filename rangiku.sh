eac6933ccfdd20a888cc7eb5a009e3f492389717
Kisuke Urahara <59564878+ki5uk3@users.noreply.github.com>
2020-03-27 18:51:17 +0530

    Update rangiku.sh

--- rangiku.sh
+++ rangiku.sh
@@ -8,25 +8,28 @@ mkdir ~/rangiku/domains/$1
 cd ~/rangiku/domains/$1
 tput setaf 2; echo "____________RUNNING FINDOMAIN__________"
 tput sgr0
-fdm -t $1 -o
+findomain-linux -t $1 -o
+tput setaf 2; echo "____________RUNNING KNOCKPY__________"
+tput sgr0
+knockpy -c $1
 tput setaf 2; echo "____________RUNNING SUBFINDER____________"
 tput sgr0
-subfinder -d $1 -o ~/rangiku/domains/$1/subfinder.txt
+subfinder -d $1 -t 100 -timeout 15 -o ~/rangiku/domains/$1/subfinder.txt
 tput setaf 2; echo "____________SORTING DOMAINS____________"
 tput sgr0
-cat ~/rangiku/domains/$1/*.txt| sort -u >> ~/rangiku/domains/$1/uniq.txt
+cat ~/rangiku/domains/$1/*.txt|sort -u|uniq -u >> ~/rangiku/domains/$1/uniq.txt
 tput setaf 2; wc -l uniq.txt
 tput sgr0
 tput setaf 2; echo "_____________RUNNING HTTPROBE____________"
 tput sgr0
-cat uniq.txt|httprobe > probe.txt
+cat uniq.txt|httprobe -c 40 > probe.txt
 tput setaf 2; wc -l probe.txt
-cat probe.txt|awk '{gsub("http://" , "");gsub("https://" , "");print}' > fornmap.txt
+cat probe.txt|awk '{gsub("http://" , "");gsub("https://" , "");print}'|sort -u|uniq -u > fornmap.txt
 tput setaf 2; echo "_____________RUNNING NMAP____________"
 tput sgr0
-nmap -F -iL fornmap.txt
-tput setaf 2; echo "__________RUNNING AQUATONE FOR SCREENSHOTS_________"
+nmap -F -iL fornmap.txt -oN ~/rangiku/domains/$1/nmapreport.txt
+tput setaf 2; echo "__________RUNNING AQUATONE FOR WEB SCREENSHOTS_________"
 tput sgr0
 cat fornmap.txt|aquatone
-tput setaf 2; echo "________________JOB DONE :)________________"
+tput setaf 2; echo "__________JOB DONE :)_Go_to ~/rangiku/domains_TO_SEE_RESULT________________"
 tput sgr0
